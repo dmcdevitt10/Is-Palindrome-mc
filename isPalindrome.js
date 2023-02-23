157ca1b3672fbd6b518f45052766fcd1c64fa7c4
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-02-23 10:04:50 -0700

    problem solved

---
+++ isPalindrome.js
@@ -0,0 +1,12 @@
+// Write your code below
+function isPalindrome(word){
+    for(let i = 0; i < word.length/2; i++){
+       if(word[i] !== word[word.length - 1 - i]){
+        return false
+       }
+    }
+    return true
+}
+
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('monday'))
