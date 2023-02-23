23ac5adc4c1c03921e69dc5b64af9fa91fb76210
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-02-23 10:29:36 -0700

    Solved with different answers

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,4 +1,4 @@
-// Write your code below
+//Answer #1
 function isPalindrome(word){
     for(let i = 0; i < word.length/2; i++){
        if(word[i] !== word[word.length - 1 - i]){
@@ -10,3 +10,33 @@ function isPalindrome(word){
 
 console.log(isPalindrome('racecar'))
 console.log(isPalindrome('monday'))
+
+
+// Answer #2
+function isPalindrome1(word){
+    return word.split('').reverse().join('') === word
+}
+
+console.log(isPalindrome1('hello'))
+console.log(isPalindrome1('racecar'))
+
+
+//Answer #3
+function isPalindrome3(string){
+    let word = string.split('')
+    let forwards = []
+    let backwards = []
+    for(let i = 0; i < word.length; i++){
+        backwards.push(word[i])
+        forwards.unshift(word[i])
+    }
+    fString = forwards.join('')
+    bString = backwards.join('')
+    if(fString === bString){
+        return true
+    }
+    return false
+}
+
+console.log(isPalindrome3('hello'))
+console.log(isPalindrome3('tacocat'))
